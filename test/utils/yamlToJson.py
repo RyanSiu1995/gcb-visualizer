90e647cf72328f60bb3e625f398d20f98fc5843e
Ryan SIU <ryan.siu@cloverhealth.com>
2020-04-18 19:43:55 +0800

    Add docs to yamlToJson utils

--- test/utils/yamlToJson.py
+++ test/utils/yamlToJson.py
@@ -1,15 +1,34 @@
 import sys
 import os
 import yaml, json
+from typing import List
 
-def isYaml(file: str):
+
+def isYaml(file: str) -> bool:
+    # Check if the file has a yaml extension
+    #
+    # Parameters:
+    #   file (str): target filename
+    #
+    # Returns:
+    #   isYaml (bool)
+
     _, fileExt = os.path.splitext(file)
     return fileExt.lower() == ".yaml" or fileExt.lower() == ".yml"
 
-def listdir_fullpath(d):
+def listdir_fullpath(d: str) -> List(str):
+    # A better listdir function which returns the full path for list dir function
+    #
+    # Parameters:
+    #   d (str): target dirname
+    #
+    # Returns:
+    #   listOfFileNames (List(str)): List of file names with full path
+
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 if __name__ == "__main__":
+    '''A cli to convert all the YAML files on dir to JSON files'''
     if len(sys.argv) != 2:
         print("Please provide the dir containing the yaml test files")
         sys.exit(1)
